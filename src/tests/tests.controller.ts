3debfcfc41eab76f60e31ef2198f53bf082b8a23
fahamdev <faham.dev@gmail.com>
2021-12-04 01:29:00 +0600

    Add DB and test module

---
+++ src/tests/tests.controller.ts
@@ -0,0 +1,44 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TestsService } from './tests.service';
+import { CreateTestDto } from './dto/create-test.dto';
+import { UpdateTestDto } from './dto/update-test.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller({ path: 'tests', version: '1' })
+@ApiTags('Tests')
+export class TestsController {
+  constructor(private readonly testsService: TestsService) {}
+
+  @Post()
+  create(@Body() createTestDto: CreateTestDto) {
+    return this.testsService.create(createTestDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.testsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.testsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTestDto: UpdateTestDto) {
+    return this.testsService.update(+id, updateTestDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.testsService.remove(+id);
+  }
+}
