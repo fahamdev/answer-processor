c4d574a62adb8dc30328bc8a44f8c507e232d0bf
fahamdev <faham.dev@gmail.com>
2021-12-05 03:04:05 +0600

    fix tests

--- src/file/file.controller.spec.ts
+++ src/file/file.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { mockFilesService } from './mocks/files.mock';
 
 describe('FileController', () => {
   let controller: FileController;
@@ -8,7 +9,12 @@ describe('FileController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FileController],
-      providers: [FileService],
+      providers: [
+        {
+          provide: FileService,
+          useValue: mockFilesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<FileController>(FileController);
