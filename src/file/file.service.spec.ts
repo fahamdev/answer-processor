78798cde459b4d65bac1fda7106dbc813996011d
fahamdev <faham.dev@gmail.com>
2021-12-06 02:26:33 +0600

    fix tests

--- src/file/file.service.spec.ts
+++ src/file/file.service.spec.ts
@@ -4,20 +4,27 @@ import {
   createMockRepository,
   MockRepository,
 } from '../common/mocks/base-repository.mock';
+import { TestsService } from '../tests/tests.service';
 import { File } from './entities/file.entity';
 import { FileService } from './file.service';
 
 describe('FileService', () => {
   let service: FileService;
   let filesRepository: MockRepository;
+  let mockTestsService: Partial<TestsService>;
 
   beforeEach(async () => {
+    mockTestsService = {};
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         FileService,
         {
           provide: getRepositoryToken(File),
           useValue: createMockRepository(),
+        },
+        {
+          provide: TestsService,
+          useValue: mockTestsService,
         },
       ],
     }).compile();
